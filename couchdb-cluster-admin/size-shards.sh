fab88b98f198f27ffcf13c50ee4aa14d414bb1bb
Daniel Roberts <droberts@dimagi.com>
2017-10-31 17:17:29 -0400

    remove size-shards.sh bash script
    suggest_shard_allocation now estimates this itself based on the size of the db

--- couchdb-cluster-admin/size-shards.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# usage:
-#   bash couchdb-cluster-admin/size-shards.sh <remote-ip> <remote-data-dir>
-
-ip=$1
-data_dir=$2
-ssh ${ip} "cd ${data_dir}/shards; find . -type f | xargs ls -alS | xargs -L 1 echo | cut -d' ' -f5,9 | sed 's|\./||g'"
