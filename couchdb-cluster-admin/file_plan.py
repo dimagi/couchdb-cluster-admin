ac1bfc4132d2eae39cb5b0f2b0e6529a19a04261
Daniel Roberts <droberts@dimagi.com>
2017-11-06 13:16:30 -0500

    add command for listing db files to rsync

--- couchdb-cluster-admin/file_plan.py
+++ couchdb-cluster-admin/file_plan.py
@@ -1,8 +1,7 @@
 import argparse
 from collections import defaultdict
 import json
-from utils import get_arg_parser, get_config_from_args, get_shard_allocation, set_up_parser
-from doc_models import ShardAllocationDoc
+from utils import get_config_from_args, get_shard_allocation, set_up_parser
 from describe import print_shard_table
 
 
@@ -26,24 +25,30 @@ def update_shard_allocation_docs_from_plan(shard_allocation_docs, plan):
     return shard_allocation_docs
 
 
-def figure_out_what_you_can_delete(plan):
+def figure_out_what_you_can_and_cannot_delete(config, plan, shard_suffix_by_db_name=None):
+    if shard_suffix_by_db_name is None:
+        shard_suffix_by_db_name = defaultdict(lambda: '.*')
     all_files = set()
     important_files_by_node = defaultdict(set)
     for db_name, by_range in plan.items():
         for shard, nodes in by_range.items():
             for node in nodes:
-                couch_file = 'shards/{shard}/{db_name}.*.couch'.format(
-                    shard=shard, db_name=db_name)
-                view_file = '.shards/{shard}/{db_name}.*_design'.format(
-                    shard=shard, db_name=db_name)
-                important_files_by_node[node].update([couch_file, view_file])
-                all_files.update([couch_file, view_file])
+                couch_file = 'shards/{shard}/{db_name}{shard_suffix}.couch'.format(
+                    shard=shard, db_name=db_name, shard_suffix=shard_suffix_by_db_name[db_name])
+                view_file = '.shards/{shard}/{db_name}{shard_suffix}_design'.format(
+                    shard=shard, db_name=db_name, shard_suffix=shard_suffix_by_db_name[db_name])
+
+                important_files_by_node[node].add(couch_file)
+                all_files.add(couch_file)
+
+                important_files_by_node[node].add(view_file)
+                all_files.add(view_file)
 
     deletable_files_by_node = {}
     for node, important_files in important_files_by_node.items():
         deletable_files_by_node[node] = all_files - important_files
 
-    return deletable_files_by_node
+    return important_files_by_node, deletable_files_by_node
 
 
 def show_plan(config, plan):
@@ -52,10 +57,21 @@ def show_plan(config, plan):
     print_shard_table(shard_allocation_docs)
 
 
-def run_plan_prune(plan, node):
-    deletable_files_by_node = figure_out_what_you_can_delete(plan)
-
+def run_plan_prune(config, plan, node):
+    _, deletable_files_by_node = figure_out_what_you_can_and_cannot_delete(
+        config, plan, shard_allocation_docs)
     for filename in sorted(deletable_files_by_node[node]):
+        print filename
+
+
+def run_important_plan(config, plan, node):
+    shard_suffix_by_db_name = {
+        db_name: get_shard_allocation(config, db_name).usable_shard_suffix
+        for db_name in plan
+    }
+    important_files_by_node, _ = figure_out_what_you_can_and_cannot_delete(
+        config, plan, shard_suffix_by_db_name)
+    for filename in sorted(important_files_by_node[node]):
         print filename
 
 
@@ -69,6 +85,11 @@ def main():
     ), subparsers.add_parser(
         'show-plan',
         help=u"Just print the shard allocation table"
+    ), subparsers.add_parser(
+        'important',
+        help=u"List files that must be present and up to date on a node "
+             u"before it is safe to commit the plan. "
+             u"(May list files that already exist on the node.)"
     )]
     for subparser in subparser_list:
         set_up_parser(subparser)
@@ -87,7 +108,10 @@ def main():
         show_plan(config, plan)
 
     if args.command == 'prune':
-        run_plan_prune(plan, config.get_formal_node_name(args.node))
+        run_plan_prune(config, plan, config.get_formal_node_name(args.node))
+
+    if args.command == 'important':
+        run_important_plan(config, plan, config.get_formal_node_name(args.node))
 
 
 if __name__ == '__main__':
