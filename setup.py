0494e6ad172af9c25bdb0adb883afad0394f7dac
Daniel Miller <dmiller@dimagi.com>
2021-03-29 11:26:44 -0400

    Drop support for Python 2

--- setup.py
+++ setup.py
@@ -69,8 +69,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
